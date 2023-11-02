1b1d23441acc85a1e1f8986147dfabdfd1a9d975
Felipe Perez Junqueira <felipetiuzanata@gmail.com>
2023-11-02 10:35:15 -0300

    Config eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'budget-planner'`, () => {
+  it('should have as title \'budget-planner\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('budget-planner');
