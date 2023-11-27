31c863b5d18791fa0afc20e49c097630fbd3677e
Felipe Perez Junqueira <felipetiuzanata@gmail.com>
2023-11-27 16:50:47 -0300

    Preparing app for adding authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {
-    path: './',
-    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'flow',
+        loadChildren: () =>
+          import('./main/cash-flow/cash-flow.module').then((m) => m.CashFlowModule),
+      },
+    ],
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
-  }
+  },
 ];
 
 @NgModule({
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -5,7 +5,7 @@ import { passwordMustMatch } from './password.validator';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
